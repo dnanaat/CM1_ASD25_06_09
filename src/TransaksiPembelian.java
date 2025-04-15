f5e30b32a5ffb94195ae2e83d92c593241d6c3f2
Fahrulkz <fahrulzaki10@gmail.com>
2025-04-15 09:49:17 +0700

    Pembuatan_TransaksiPembelian

--- src/TransaksiPembelian.java
+++ src/TransaksiPembelian.java
@@ -1,3 +1,23 @@
 public class TransaksiPembelian {
-    
+    String kodeTransaksi;
+    String namaPembeli;
+    String tanggalPembelian;
+    int qty;
+    Barang brg;
+
+    public TransaksiPembelian(String kodeTransaksi, String namaPembeli, String tanggalPembelian, int qty, Barang brg) {
+        this.kodeTransaksi = kodeTransaksi;
+        this.namaPembeli = namaPembeli;
+        this.tanggalPembelian = tanggalPembelian;
+        this.qty = qty;
+        this.brg = brg;
+
+    }
+
+    void tampilDataTransaksi() {
+        System.out.println("Kode Transaksi: " + kodeTransaksi);
+        System.out.println("Nama Pembeli: " + namaPembeli);
+        System.out.println("Tanggal Pembelian: " + tanggalPembelian);
+        System.out.println("Jumlah: " + qty);
+    }
 }
