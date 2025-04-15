46fdc80b7c20d0a7ca6cd8475a506a8011eff2b1
dnanaat <daanahafidhilislam@gmail.com>
2025-04-15 09:24:54 +0700

    Penambahan Data dan Tampil Data Transaksi

--- src/Barang.java
+++ src/Barang.java
@@ -1,3 +1,27 @@
-public class barang {
-    
+public class Barang {
+    String kodeBarang;
+    String nama;
+    String kategori;
+    int stock;
+    int harga;
+
+    Barang(){
+
+    }
+
+    Barang(String kodeBarang, String nama, String kategori, int stock, int harga) {
+        this.kodeBarang = kodeBarang;
+        this.nama = nama;
+        this.kategori = kategori;
+        this.stock = stock;
+        this.harga= harga;
+    }
+
+    void tampilDataTransaksi(){
+        System.out.println("Kode Barang : " + kodeBarang);
+        System.out.println("Nama        : " + nama);
+        System.out.println("Kategori    : " + kategori);
+        System.out.println("Stock       : " + stock);
+        System.out.println("Harga       : " + harga);
+    }
 }
