fc5654d4df87b3aeee3a271bb887c663cb76047b
dnanaat <daanahafidhilislam@gmail.com>
2025-04-15 10:59:10 +0700

    Penambahan Data

--- src/MainSistemProject.java
+++ src/MainSistemProject.java
@@ -4,6 +4,25 @@ public class MainSistemProject {
         
         Scanner sc = new Scanner(System.in);
 
+        Barang brg1 = new Barang("Br01", "Leo Keripik", "Makanan", 10, 1000);
+        Barang brg2 = new Barang("Br02", "You C1000 Botol", "Minuman", 10, 7000);
+        Barang brg3 = new Barang("Br03", "Teh Pucuk Botol", "Minuman", 10, 4000);
+        Barang brg4 = new Barang("Br04", "Rinso Cair", "Sabun", 10, 7000);
+        Barang brg5 = new Barang("Br05", "Mama Lemon Cair", "Sabun", 10, 1000);
+
+        TransaksiPembelian tr1 = new TransaksiPembelian("Tr001", "Santi", "21-04-2024", 1, brg1);
+        TransaksiPembelian tr2 = new TransaksiPembelian("Tr002", "Vani", "21-04-2024", 1, brg2);
+        TransaksiPembelian tr3 = new TransaksiPembelian("Tr003", "Siska", "21-04-2024", 1, brg3);
+        TransaksiPembelian tr4 = new TransaksiPembelian("Tr004", "MeiMei", "21-04-2024", 1, brg4);
+        TransaksiPembelian tr5 = new TransaksiPembelian("Tr005", "Ihsan", "21-04-2024", 1, brg5);
+
+        ServiceClass serviceClass = new ServiceClass();
+        serviceClass.tambahDataTransaksi(tr1);
+        serviceClass.tambahDataTransaksi(tr2);
+        serviceClass.tambahDataTransaksi(tr3);
+        serviceClass.tambahDataTransaksi(tr4);
+        serviceClass.tambahDataTransaksi(tr5);
+
         while (true) {
             System.out.println("============== TOKO Manasuka ==============");
             System.out.println("BANK MENU : ");
