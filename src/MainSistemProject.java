a9db0d4e2d2ec85dd9fdba19e10793b14e02f8fc
dnanaat <daanahafidhilislam@gmail.com>
2025-04-15 10:04:24 +0700

    Pembuatan Class Main

--- src/MainSistemProject.java
+++ src/MainSistemProject.java
@@ -4,8 +4,42 @@ public class MainSistemProject {
         
         Scanner sc = new Scanner(System.in);
 
-        
+        while (true) {
+            System.out.println("============== TOKO Manasuka ==============");
+            System.out.println("BANK MENU : ");
+            System.out.println("1. Data Barang");
+            System.out.println("2. Data Transaksi");
+            System.out.println("3. Pencarian Data");
+            System.out.println("4. Pengurutan Data");
+            System.out.println("5. Exit");
+            System.out.print("Masukkan pilihan menu : ");
+            int menu = sc.nextInt();
 
+            if (menu == 1) {
+                
+            }
+
+            else if (menu == 2) {
+                
+            }
+
+            else if (menu == 3) {
+                
+            }
+
+            else if (menu == 4) {
+                
+            }
+
+            else if (menu == 5) {
+                
+            }
+
+            else {
+                System.out.println("Pilihan menu tidak tersedia!\n");
+            }
+
+        }
 
     }
 }
